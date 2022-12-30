d609a227d69828f7ef4ac3a14b53aa2579e999e9
YOUR NAME <YOUR EMAIL@SAMLECOM>
2022-12-30 15:36:33 +0700

    berjuang

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
 </head>
